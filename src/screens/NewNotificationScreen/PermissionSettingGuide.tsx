f197956338b6efb3446d373deb901add185cd0ce
youngjin <masichall@gmail.com>
2020-10-03 21:25:24 +0900

    feat: 알림 권한 안내화면 디자인 개선

--- src/screens/NewNotificationScreen/PermissionSettingGuide.tsx
+++ src/screens/NewNotificationScreen/PermissionSettingGuide.tsx
@@ -1,23 +1,30 @@
 import * as Linking from 'expo-linking';
 import React from 'react';
-import { Platform } from 'react-native';
 import styled from 'styled-components/native';
-import Button from '../../components/Button';
+
+import Button, { ButtonVariant } from '../../components/Button';
 import ScreenHeader from './ScreenHeader';
 
+const openSettings = () => {
+  Linking.openSettings();
+};
+
 function PermissionSettingGuide(): React.ReactElement {
-  const openSettings = () => {
-    if (Platform.OS === 'ios') {
-      Linking.openURL('app-settings:');
-    } else {
-      Linking.openSettings();
-    }
-  };
   return (
     <Container>
       <ScreenHeader />
-      <Text>알림을 보낼 수 있는 권한이 필요해요.</Text>
-      <Button label="권한 설정" onPress={openSettings} />
+      <Main>
+        <Title>알림을 보낼 수 있는 권한이 없어요.</Title>
+        <Description>아래 버튼을 눌러 알림 권한을 활성화 해주세요.</Description>
+        <ButtonWrapper>
+          <Button
+            label="설정화면으로 이동"
+            onPress={openSettings}
+            variant={ButtonVariant.PrimaryTenderContained}
+            style={{ paddingHorizontal: 16 }}
+          />
+        </ButtonWrapper>
+      </Main>
     </Container>
   );
 }
@@ -26,6 +33,25 @@ const Container = styled.SafeAreaView`
   flex: 1;
 `;
 
-const Text = styled.Text``;
+const Main = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonWrapper = styled.View`
+  margin-top: 16px;
+`;
+
+const Title = styled.Text`
+  color: ${(props) => props.theme.colors.typography.title};
+  font-size: 16px;
+`;
+
+const Description = styled.Text`
+  margin-top: 8px;
+  color: ${(props) => props.theme.colors.typography.secondary};
+  font-size: 14px;
+`;
 
 export default PermissionSettingGuide;
