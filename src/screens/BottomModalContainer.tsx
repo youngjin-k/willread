ebc6f85b76d74611285889240d97879f0202b0ab
youngjin <masichall@gmail.com>
2020-10-04 16:33:53 +0900

    feat: BottomModalContainer 최대 사이즈 지정

--- src/screens/BottomModalContainer.tsx
+++ src/screens/BottomModalContainer.tsx
@@ -11,17 +11,25 @@ function BottomModalContainer({
 }: BottomModalContainerProps): React.ReactElement {
   return (
     <BottomModalContainerBlock>
-      <ModalHandle />
-      {children}
+      <Main>
+        <ModalHandle />
+        {children}
+      </Main>
     </BottomModalContainerBlock>
   );
 }
 
-const BottomModalContainerBlock = styled.SafeAreaView`
+const BottomModalContainerBlock = styled.View`
+  align-items: center;
+`;
+
+const Main = styled.SafeAreaView`
   background-color: ${(props) => props.theme.colors.background};
   border-top-left-radius: 16px;
   border-top-right-radius: 16px;
   padding-bottom: 16px;
+  max-width: 560px;
+  width: 100%;
 `;
 
 export default BottomModalContainer;
