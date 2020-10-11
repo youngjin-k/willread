697d6905b2599e24442b627da7724695964a1852
youngjin <masichall@gmail.com>
2020-10-11 13:44:55 +0900

    feat: filter 제거

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -2,21 +2,22 @@ import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import * as Notifications from 'expo-notifications';
 import React, { useEffect, useRef, useState } from 'react';
-import { Image, ScrollView, useColorScheme } from 'react-native';
-import { useSelector } from 'react-redux';
+import {
+  Image, ScrollView, useColorScheme, View,
+} from 'react-native';
 import styled from 'styled-components/native';
 
 import willreadDark from '../../../assets/willread-dark.png';
 import willreadLight from '../../../assets/willread-light.png';
 import ArticleCard from '../../components/articleCard/ArticleCard';
 import ArticleListCard from '../../components/articleCard/ArticleListCard';
-import CategoryFilter from '../../components/CategoryFilter';
 import { RootStackParamList, TabParamList } from '../../config/Navigation';
 import { Article } from '../../features/article/articles';
 import { CategoryColors } from '../../features/homeCategoryFilters';
-import { RootState } from '../../features/store';
 import BottomModal from '../../components/BottomModal';
 import ArticleMenu from './ArticleMenu';
+import useArticle from '../../features/article/useArticle';
+import Line from '../../components/Line';
 
 const recommendItem: Article = {
   id: '',
@@ -30,11 +31,7 @@ const recommendItem: Article = {
 function HomeScreen(): React.ReactElement {
   const scrollViewRef = useRef<ScrollView>();
   const scheme = useColorScheme();
-  const { articles } = useSelector((state: RootState) => state.articles);
-  const { filters } = useSelector(
-    (state: RootState) => state.homeCategoryFilters,
-  );
-  const [displayArticles, setDisplayArticles] = useState<Article[]>([]);
+  const { articles } = useArticle();
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
   const [selectedArticle, setSelectedArticle] = useState<Article>();
   const route = useRoute<RouteProp<TabParamList, 'Home'>>();
@@ -61,16 +58,6 @@ function HomeScreen(): React.ReactElement {
     );
     return () => subscription.remove();
   }, [navigation]);
-
-  useEffect(() => {
-    if (filters.category === CategoryColors.DEFAULT) {
-      setDisplayArticles(articles);
-      return;
-    }
-    setDisplayArticles(
-      articles.filter((article) => article.categoryColor === filters.category),
-    );
-  }, [articles, filters]);
 
   const handlePressArticle = (article: Article) => {
     navigation.navigate('Viewer', {
@@ -100,14 +87,19 @@ function HomeScreen(): React.ReactElement {
             source={scheme === 'dark' ? willreadDark : willreadLight}
           />
         </Header>
+
         <ArticleCard
           article={recommendItem}
           onPress={handlePressArticle}
           onLongPress={handleLongPressArticle}
         />
-        <CategoryFilter />
-        {displayArticles
-          && displayArticles.map((article) => (
+
+        <View style={{ paddingVertical: 16 }}>
+          <Line />
+        </View>
+
+        {articles
+          && articles.map((article) => (
             <ArticleListCard
               key={article.id}
               article={article}
