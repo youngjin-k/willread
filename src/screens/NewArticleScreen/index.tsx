bca36a79b6fc4b848adff5e8b9b0dc941248cb1f
youngjin-k <masichall@gmail.com>
2020-12-24 01:35:59 +0900

    feat: 아티클 등록 개선

--- src/screens/NewArticleScreen/index.tsx
+++ src/screens/NewArticleScreen/index.tsx
@@ -2,7 +2,9 @@ import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { getLinkPreview } from 'link-preview-js';
 import React, { useCallback, useEffect, useState } from 'react';
-import { KeyboardAvoidingView, Platform, Text } from 'react-native';
+import {
+  KeyboardAvoidingView, Platform, Text, useColorScheme,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import styled from 'styled-components/native';
 
@@ -12,44 +14,46 @@ import TextInput from '../../components/TextInput';
 import { RootStackParamList } from '../../config/Navigation';
 import useArticle from '../../features/article/useArticle';
 import VALID_URL from '../../lib/regex/validUrl';
+import themes from '../../lib/styles/themes';
 import Actions from './Actions';
 import Complete from './Complete';
 
 function NewArticleScreen(): React.ReactElement {
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
-  const { articleDraft, setArticleDraft, addArticle } = useArticle();
+  const { addArticle } = useArticle();
   const [loading, setLoading] = useState(false);
   const [disabled, setDisabled] = useState(true);
   const [complete, setComplete] = useState(false);
   const route = useRoute<RouteProp<RootStackParamList, 'NewArticle'>>();
+  const [link, setLink] = useState('');
+  const scheme = useColorScheme();
+  const [useExpandedLinkInput, setUseExpandedLinkInput] = useState(false);
 
   useEffect(() => {
     if (route?.params?.uri) {
-      setArticleDraft({
-        ...articleDraft,
-        uri: route.params.uri,
-      });
+      setLink(route.params.uri);
     }
   }, [route]);
 
   useEffect(() => {
-    setDisabled(!VALID_URL.test(articleDraft.uri));
-  }, [articleDraft]);
+    setDisabled(!VALID_URL.test(link));
+  }, [link]);
 
-  const handleTextChange = (uri: string) => {
-    setArticleDraft({
-      ...articleDraft,
-      uri,
-    });
+  const handleChangeLink = (uri: string) => {
+    setLink(uri);
+  };
+
+  const handleExpandButtonClick = () => {
+    setUseExpandedLinkInput(true);
   };
 
   const handleOnPress = useCallback(async () => {
     setLoading(true);
 
     try {
-      const response = (await getLinkPreview(articleDraft.uri)) as any;
+      const response = (await getLinkPreview(link)) as any;
       addArticle({
-        uri: articleDraft.uri,
+        uri: link,
         title: response.title,
         description: response.description,
         image: response.images.length > 0 ? response.images[0] : '',
@@ -59,7 +63,7 @@ function NewArticleScreen(): React.ReactElement {
     } catch (e) {
       setLoading(false);
     }
-  }, [articleDraft, addArticle]);
+  }, [link, addArticle]);
 
   return (
     <Container>
@@ -85,15 +89,42 @@ function NewArticleScreen(): React.ReactElement {
             <Complete />
           </Content>
         ) : (
-
           <Content>
-            <FormLabel>링크를 입력하세요</FormLabel>
-            <TextInput
-              defaultValue={route?.params?.uri}
-              autoFocus
-              keyboardType="url"
-              onChangeText={handleTextChange}
-            />
+            {useExpandedLinkInput ? (
+              <LinkInput
+                placeholder="링크를 입력하세요"
+                placeholderTextColor={
+                  themes[scheme === 'dark' ? 'dark' : 'light'].colors.typography
+                    .secondary
+                }
+                autoFocus
+                keyboardType="url"
+                onChangeText={handleChangeLink}
+                defaultValue={link}
+                value={link}
+                multiline
+                textAlignVertical="top"
+              />
+            ) : (
+              <>
+                <FormLabel>링크를 입력하세요</FormLabel>
+                <TextInput
+                  autoFocus
+                  keyboardType="url"
+                  onChangeText={handleChangeLink}
+                />
+                <ExpandButtonWrapper>
+                  <Button
+                    onPress={handleExpandButtonClick}
+                    size={ButtonSize.Small}
+                    variant={ButtonVariant.PrimaryTenderContained}
+                    style={{ paddingHorizontal: 8 }}
+                  >
+                    <MaximizeIcon name="maximize-2" />
+                  </Button>
+                </ExpandButtonWrapper>
+              </>
+            )}
 
             <Actions>
               <Button
@@ -148,6 +179,23 @@ const Content = styled.View`
   flex: 1;
   justify-content: center;
   padding: 0 16px 72px 16px;
+`;
+
+const LinkInput = styled.TextInput`
+  flex: 1;
+  font-size: 18px;
+  color: ${(props) => props.theme.colors.typography.title};
+  padding: 0;
+`;
+
+const ExpandButtonWrapper = styled.View`
+  align-items: flex-end;
+  padding: 12px 0;
+`;
+
+const MaximizeIcon = styled(Icon)`
+  font-size: 20px;
+  color: ${(props) => props.theme.colors.primary};
 `;
 
 export default NewArticleScreen;
