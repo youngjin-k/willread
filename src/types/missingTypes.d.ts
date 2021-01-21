b10a93b4db1e6b7bcbdaa2d750b15d2c5e54bda3
youngjin-k <masichall@gmail.com>
2021-01-21 23:53:40 +0900

    feat: type definition 추가

--- src/types/missingTypes.d.ts
+++ src/types/missingTypes.d.ts
@@ -1,13 +1,2 @@
 declare module '*.png';
 declare module '*.jpg';
-
-declare namespace urlRegexSafe {
-  interface Options {
-    readonly exact?: boolean;
-    readonly strict?: boolean;
-  }
-}
-
-declare function urlRegexSafe(options?: urlRegexSafe.Options): RegExp;
-
-export = urlRegexSafe;
