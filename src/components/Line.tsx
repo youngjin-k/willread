91871a3da3c7bd65c3dbd0fde7f4bd6fc7905704
youngjin-k <masichall@gmail.com>
2021-02-01 23:54:58 +0900

    feat: margin 제어기능 추가 구현

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -1,28 +1,47 @@
 import React, { ReactElement } from 'react';
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
 export interface LineProps {
-    padding?: number;
+  padding?: number;
+  marginTop?: number;
+  marginBottom?: number;
 }
 
-function Line({ padding = 16 }: LineProps): ReactElement {
+function Line({
+  padding = 16,
+  marginTop = 0,
+  marginBottom = 0,
+}: LineProps): ReactElement {
   return (
-    <LineBlock padding={padding}>
+    <LineBlock
+      padding={padding}
+      marginTop={marginTop}
+      marginBottom={marginBottom}
+    >
       <Hr />
     </LineBlock>
   );
 }
 
-const LineBlock = styled.View<LineProps>`
-    width: 100%;
-    height: 1px;
-    padding: 0 ${(props) => props.padding}px;
+const LineBlock = styled.View<Required<LineProps>>`
+  width: 100%;
+  height: 1px;
+  padding: 0 ${(props) => props.padding}px;
+
+  ${(props) => props.marginTop > 0
+    && css`
+      margin-top: ${props.marginTop}px;
+    `}
+  ${(props) => props.marginBottom > 0
+    && css`
+      margin-bottom: ${props.marginBottom}px;
+    `}
 `;
 
 const Hr = styled.View`
-    width: 100%;
-    height: 1px;
-    background-color: ${(props) => props.theme.colors.border};
+  width: 100%;
+  height: 1px;
+  background-color: ${(props) => props.theme.colors.border};
 `;
 
 export default Line;
