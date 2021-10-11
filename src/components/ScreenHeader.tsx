aa81d2a240dab9ec110459601c7ed1d21afc7f73
youngjin-k <masichall@gmail.com>
2021-10-12 00:02:41 +0900

    fix: 헤더 오류 개선

--- src/components/ScreenHeader.tsx
+++ src/components/ScreenHeader.tsx
@@ -1,6 +1,7 @@
-import React, { ReactNode } from 'react';
-import { StyleSheet } from 'react-native';
-import styled, { css } from '@emotion/native';
+import React, { memo, ReactNode } from 'react';
+import { StyleSheet, View } from 'react-native';
+import styled from '@emotion/native';
+import useTheme from '../lib/styles/useTheme';
 
 export interface ScreenHeaderProps {
   isScrolled?: boolean;
@@ -8,26 +9,27 @@ export interface ScreenHeaderProps {
 }
 
 function ScreenHeader({ isScrolled = false, children }: ScreenHeaderProps) {
+  const theme = useTheme();
+
   return (
-    <ScreenHeaderBlock isScrolled={isScrolled}>{children}</ScreenHeaderBlock>
+    <View
+      style={{
+        height: 56,
+        backgroundColor: theme.colors.background,
+        paddingTop: 0,
+        paddingBottom: 8,
+        paddingHorizontal: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomColor: isScrolled ? theme.colors.border : theme.colors.background,
+      }}
+    >
+      {children}
+    </View>
   );
 }
-
-const ScreenHeaderBlock = styled.View<{ isScrolled: boolean }>`
-  height: 56px;
-  background-color: ${(props) => props.theme.colors.background};
-  padding: 0 16px 8px 16px;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: flex-end;
-  border-bottom-width: ${StyleSheet.hairlineWidth}px;
-  border-bottom-color: ${(props) => props.theme.colors.background};
-
-  ${(props) => props.isScrolled
-    && css`
-      border-bottom-color: ${props.theme.colors.border};
-    `}
-`;
 
 export const ScreenHeaderTitle = styled.Text`
   font-weight: bold;
@@ -35,4 +37,4 @@ export const ScreenHeaderTitle = styled.Text`
   color: ${(props) => props.theme.colors.typography.primary};
 `;
 
-export default ScreenHeader;
+export default memo(ScreenHeader);
